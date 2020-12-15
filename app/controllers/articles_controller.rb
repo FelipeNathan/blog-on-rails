d63aba0838b1e15cc2cf0862a644b2d09ed6df3e
felipenathan <felipe.nathan@totvs.com.br>
2020-12-15 15:15:33 -0300

    Tagging feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,44 +1,45 @@
 class ArticlesController < ApplicationController
-    include ArticlesHelper
-    before_action :find_article, only: [:edit, :show, :update, :destroy]
+  include ArticlesHelper
+  before_action :find_article, only: [:edit, :show, :update, :destroy]
 
-    def index
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def create
-        @article = Article.new(article_params)
-        @article.save
+  def create
+    @article = Article.new(article_params)
+    @article.save
 
-        flash.notice = "Article '#{@article.title}' Created!"
+    flash.notice = "Article '#{@article.title}' Created!"
 
-        redirect_to article_path(@article)
-    end
+    redirect_to article_path(@article)
+  end
 
-    def new
-        @article = Article.new
-    end
+  def new
+    @article = Article.new
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def show
-        @comment = Comment.new
-    end
+  def show
+    @comment = Comment.new
+  end
 
-    def update
-        @article.update(article_params)
-        
-        flash.notice = "Article '#{@article.title}' Updated!"
-        
-        redirect_to article_path(@article)
-    end
+  def update
+    @article.update(article_params)
+    
+    flash.notice = "Article '#{@article.title}' Updated!"
+    
+    redirect_to article_path(@article)
+  end
 
-    def destroy
-        @article.destroy
+  def destroy
+    
+    @article.destroy
 
-        flash.notice = "Article '#{@article.title}' Deleted!"
+    flash.notice = "Article '#{@article.title}' Deleted!"
 
-        redirect_to articles_path
-    end
+    redirect_to articles_path
+  end
 end
---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,26 @@
+class TagsController < ApplicationController
+
+  before_action :find_tag, only: [:show, :destroy]
+
+  def index 
+    @tags = Tag.all
+  end 
+
+  def show
+  end
+
+  def destroy 
+
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    redirect_to tags_path
+
+  end
+
+  private
+    def find_tag
+      @tag = Tag.find(params[:id])
+    end 
+end
